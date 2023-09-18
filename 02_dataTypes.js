0f823d8eaa06dd48a100e90d5387ef503aa9f55f
KetanDashpute <dashputeketan@gmail.com>
2023-09-18 18:08:16 +0530

    DataTypes

---
+++ 02_dataTypes.js
@@ -0,0 +1,9 @@
+"use strict"; // treat all JS code as newer version
+
+let name = "Ketan"; // string
+let age = "24"; // number
+let isLoggedIn = false; // boolean
+
+console.log(typeof name);
+console.log(typeof age);
+console.log(typeof isLoggedIn);
