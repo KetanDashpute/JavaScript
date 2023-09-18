222487f64e56aa677932ccf338acda69747be12c
KetanDashpute <dashputeketan@gmail.com>
2023-09-18 17:27:18 +0530

    Learnt Variables in JavaScript

---
+++ 01_variables.js
@@ -0,0 +1,15 @@
+const accountId = 12345;
+let accountEmail = "dashputeketan@gmail.com";
+var accountPassword = "123";
+city = "pune";
+let accountState;
+
+// accountId = 2; Not Allowed constant will not change
+
+accountEmail = "dash@mail";
+accountPassword = "456";
+city = "nashik";
+
+console.log(accountId);
+
+console.table([accountEmail, accountPassword, city, accountState]);
