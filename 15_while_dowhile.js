8badda4c1e212832cee8a92f7b0ecfb42817eac7
KetanDashpute <dashputeketan@gmail.com>
2023-09-30 19:30:31 +0530

    do-while

--- 15_while_dowhile.js
+++ 15_while_dowhile.js
@@ -4,5 +4,17 @@ while (index <= 10) {
   index = index + 1;
 }
 
-let myArray = [flash, batman, superman];
+let myArray = ["flash", "batman", "superman"];
+let arr = 0;
+while (arr < myArray.length) {
+  console.log(`${myArray[arr]}`);
+  arr++;
+}
 
+// do-while
+
+let score = 1;
+do {
+  console.log(`${score}`);
+  score = score + 1;
+} while (score <= 10);
